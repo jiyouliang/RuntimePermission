3147566377be8fffba5c290453cce68466272746
jiyouliang <18098975621@163.com>
2018-07-09 20:26:36 +0800

    Android Runtime Permission

---
+++ app/src/main/java/com/jiyouliang/runtimepermission/MainActivity.java
@@ -0,0 +1,82 @@
+package com.jiyouliang.runtimepermission;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_CODE_CALL_PHONE = 2000;
+    private Button mBtnCall;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBtnCall = (Button) findViewById(R.id.btn_call);
+
+        mBtnCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_call:
+                callPhone();
+                break;
+        }
+    }
+
+    /**
+     * 拨号
+     */
+    private void callPhone() {
+        //运行时权限检查
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //申请CALL_PHONE权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},
+                    REQUEST_CODE_CALL_PHONE);//自定义的code
+        } else {//拨号
+            showCallPhone();
+        }
+    }
+
+    private void showCallPhone() {
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        intent.setAction("android.intent.action.DIAL");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addCategory("android.intent.category.BROWSABLE");
+        intent.setData(Uri.parse("tel:" + "10086"));
+        startActivity(intent);
+    }
+
+    /**
+     * 权限回调
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_CALL_PHONE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //获取到权限，做相应处理
+            showCallPhone();
+        }
+    }
+}
